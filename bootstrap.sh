ac5cbbd10bf7796237cf5bf610202e03df88d04e
Russell Aunger <rba@live.com>
2020-03-24 13:26:21 -0400

    Python debug line trace.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,7 @@
 
 set -x
 
-/opt/pypy/bin/pypy -u /opt/bootstrap.py
+NO_TRACE_LIBS=/opt/pypy:/usr:/var:$(python -c 'import sys ; print ":".join(sys.path)[1:]')
+
+/opt/pypy/bin/pypy -u -v -m trace --ignore-dir=$NO_TRACE_LIBS --trace /opt/bootstrap.py
 echo "Exit code $?"
