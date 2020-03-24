91600f4647e18cb12786eb51f22f2b6953abb6ae
Russell Aunger <rba@live.com>
2020-03-24 13:26:43 -0400

    Return pypy exit code.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,7 @@ set -x
 NO_TRACE_LIBS=/opt/pypy:/usr:/var:$(python -c 'import sys ; print ":".join(sys.path)[1:]')
 
 /opt/pypy/bin/pypy -u -v -m trace --ignore-dir=$NO_TRACE_LIBS --trace /opt/bootstrap.py
-echo "Exit code $?"
+
+EXIT_CODE=$?
+echo "Exit code $EXIT_CODE"
+exit $EXIT_CODE
