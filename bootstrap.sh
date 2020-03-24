4d20145a0dca85478d181263db18b7ebc8ed5f05
Russell Aunger <rba@live.com>
2020-03-24 16:39:48 -0400

    Remove -v and -u pypy flags.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -x
 
 NO_TRACE_LIBS=/opt/pypy:/usr:/var:$(python -c 'import sys ; print ":".join(sys.path)[1:]')
 
-/opt/pypy/bin/pypy -u -v -m trace --ignore-dir=$NO_TRACE_LIBS --trace /opt/bootstrap.py
+/opt/pypy/bin/pypy -m trace --ignore-dir=$NO_TRACE_LIBS --trace /opt/bootstrap.py
 
 EXIT_CODE=$?
 echo "Exit code $EXIT_CODE"
